49958a17a7599a0de557bf3e83f2ac3f35fc7ef6
Louis Milrod <int4rweb@gmail.com>
2022-01-24 12:53:54 -0500

    created sales person list item

--- DealershipApi.Models/DisplayModels/SalesPerson/SalesPersonListItem.cs
+++ DealershipApi.Models/DisplayModels/SalesPerson/SalesPersonListItem.cs
@@ -8,5 +8,8 @@ namespace DealershipApi.Models.DisplayModels.SalesPerson
 {
     public class SalesPersonListItem
     {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
     }
 }
