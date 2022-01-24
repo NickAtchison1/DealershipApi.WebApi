93938cbea5ada8f7026f3ca6d81c3803d26df473
Louis Milrod <int4rweb@gmail.com>
2022-01-24 14:04:14 -0500

    added a get sales people controller

--- DealershipApi.WebApi/Controllers/SalesPersonController.cs
+++ DealershipApi.WebApi/Controllers/SalesPersonController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DealershipApi.Models.DisplayModels.SalesPerson;
+using DealershipApi.Services.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +9,24 @@ using System.Web.Http;
 
 namespace DealershipApi.WebApi.Controllers
 {
+    [Authorize]
     public class SalesPersonController : ApiController
     {
+        private SalesPersonService CreateSalesPersonService()
+        {
+            return new SalesPersonService();
+        }
+
+        public IHttpActionResult GetSalesPeople()
+        {
+            SalesPersonService s = CreateSalesPersonService();
+            var salespeople = s.GetSalesPeople();
+            return Ok(salespeople);
+        }
+
+        public IHttpActionResult CreateSalesPerson(SalesPersonCreate model)
+        {
+
+        }
     }
 }
