e524e6ab41e759785e41c4b4856c71ecc68ad652
Louis Milrod <int4rweb@gmail.com>
2022-01-24 12:34:40 -0500

    added CreateSalesPerson method

--- DealershipApi.Services/Services/SalesPersonService.cs
+++ DealershipApi.Services/Services/SalesPersonService.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DealershipApi.Models.DisplayModels.SalesPerson;
+using DealershipApi.Data.DataModels;
+
 
 namespace DealershipApi.Services.Services
 {
     public class SalesPersonService
     {
+        public SalesPersonService()
+        {
 
+        }
+
+        public bool CreateSalesPerson (SalesPersonCreate model)
+        {
+            var entity = new SalesPerson()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Email = model.Email
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.SalesPeople.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
