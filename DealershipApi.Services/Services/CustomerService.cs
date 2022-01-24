fd24e8007f68d6d69ec9eac4b960f92ba28531a1
Landon Shay <landonshay@gmail.com>
2022-01-24 14:12:46 -0500

    finished customer related things

--- DealershipApi.Services/Services/CustomerService.cs
+++ DealershipApi.Services/Services/CustomerService.cs
@@ -11,19 +11,83 @@ namespace DealershipApi.Services.Services
 {
     public class CustomerService
     {
-        public bool CreateNote(CustomerCreate customer)
+        private readonly int _userId;
+
+        public CustomerService(int userId)
+        {
+            _userId = userId;
+        }
+
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        public bool CreateCustomer(CustomerCreate customer)
         {
             var entity = new Customer()
             {
-                OwnerId = _userId,
-                Title = model.Title,
-                Content = model.Content,
-                CreatedUtc = DateTimeOffset.Now
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Address = customer.Address,
+                Email = customer.Email,
             };
 
+            _context.Customers.Add(entity);
+            return _context.SaveChanges() == 1;
+        }
+
+        public IEnumerable<CustomerListItem> GetCustomers()
+        {
             using (var ctx = new ApplicationDbContext())
             {
-                ctx.Notes.Add(entity);
+                var query = ctx.Customers.Where(e => e.Id == _userId).Select(e => new CustomerListItem
+                {
+                    CustomerId = e.Id,
+                    FirstName = e.FirstName,
+                    Address = e.Address,
+                    Email=e.Email,
+                }
+                 );
+                return query.ToArray();
+            }
+        }
+
+        public CustomerDetail GetCustomerById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Customers.Single(e => e.Id == id);
+                return new CustomerDetail
+                {
+                    CustomerId = entity.Id,
+                    FirstName = entity.FirstName,
+                    LastName = entity.LastName,
+                    Address = entity.Address,
+                    Email = entity.Email,
+                };
+            }
+        }
+
+        public bool UpdateCustomer(CustomerEdit customer)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Customers.Single(e => e.Id == customer.CustomerId);
+
+                entity.FirstName = customer.FirstName;
+                entity.LastName = customer.LastName;
+                entity.Address = customer.Address;
+                entity.Email = customer.Email;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx.Customers.Single(e => e.Id == id);
+
+                ctx.Customers.Remove(entity);
+
                 return ctx.SaveChanges() == 1;
             }
         }
