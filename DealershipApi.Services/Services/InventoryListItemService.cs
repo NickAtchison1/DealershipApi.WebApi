e3b0b424b0a4108dded9bcf2f4145cbc26b4f8a0
Louis Milrod <int4rweb@gmail.com>
2022-01-26 14:12:46 -0500

    inventory list item created

---
+++ DealershipApi.Services/Services/InventoryListItemService.cs
@@ -0,0 +1,68 @@
+﻿using DealershipApi.Data.DataModels;
+using DealershipApi.Models.DisplayModels.Dealership;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DealershipApi.Services.Services
+{
+    public class InventoryListItemService
+    {
+        public IEnumerable<InventoryListItem> GetInventoryInStock()
+        {
+            using (var ctx = new ApplicationDbContext()) 
+            {                             
+                var query = ctx.Vehicles.Select(
+                    s => new InventoryListItem
+                    {
+                        VehicleId = s.Id,
+                        VehicleName = s.VehicleName,
+                        DealershipLocation = s.Dealership.Name,
+                        InStock = s.InStock
+
+                    }).Where(i => i.InStock == true);
+
+               return query.ToList();            
+                
+            }            
+        }
+
+        public IEnumerable<InventoryListItem> GetInventoryNotInStock()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Vehicles.Select(
+                    s => new InventoryListItem
+                    {
+                        VehicleId = s.Id,
+                        VehicleName = s.VehicleName,
+                        DealershipLocation = s.Dealership.Name,
+                        InStock = s.InStock
+
+                    }).Where(i => i.InStock == false);
+
+                return query.ToList();
+            }
+        }
+
+        public IEnumerable<InventoryListItem> GetInventoryAllAvailableStock()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Vehicles.Select(
+                    s => new InventoryListItem
+                    {
+                        VehicleId = s.Id,
+                        VehicleName = s.VehicleName,
+                        DealershipLocation = s.Dealership.Name,
+                        InStock = s.InStock
+
+                    });
+
+                return query.ToList();
+            }
+        }
+    }
+}
